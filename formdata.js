df13ae991012fcda8785f5676d873734a90d2f7a
Gregory Koberger <gkoberger@gmail.com>
2011-02-11 14:42:03 -0800

    No more closures

--- formdata.js
+++ formdata.js
@@ -16,62 +16,59 @@
 //   fd.open("POST", "http://example.com");
 
 
-(function( ) {
-    var hasFormData = (typeof FormData != "undefined");
+gFormData = function(){
+    this.fields = {};
+    this.xhr = new XMLHttpRequest();
+    // Prefix the boundary with "g".
+    this.boundary = "g" + (new Date().getTime()) + "" + Math.floor(Math.random() * 10000000);
+    this.hasFormData = (typeof FormData != "undefined");
 
-    gFormData = function(){
-        this.fields = {};
-        this.xhr = new XMLHttpRequest();
-        // Prefix the boundary with "g".
-        this.boundary = "g" + (new Date().getTime()) + "" + Math.floor(Math.random() * 10000000);
+    if(this.hasFormData) {
+        this.formData = new FormData();
+    } else {
+        this.output = "";
+    }
 
-        if(hasFormData) {
-            this.formData = new FormData();
+    this.append = function(name, val) {
+        if(this.hasFormData) {
+            this.formData.append(name, val);
         } else {
-            this.output = "";
-        }
+            if(typeof val == "object" && "fileName" in val) {
+                this.output += "--" + this.boundary + "\r\n";
+                this.output += "Content-Disposition: form-data; name=\"" + name.replace(/[^\w]/g, "") + "\";";
 
-        this.append = function(name, val) {
-            if(hasFormData) {
-                this.formData.append(name, val);
+                // Encoding trick via ecmanaut (http://bit.ly/6p30c5)
+                this.output += " filename=\""+unescape(encodeURIComponent(val.fileName)) +"\";\r\n";
+                this.output += "Content-Type: " + val.type;
+
+                this.output += "\r\n\r\n";
+                this.output += val.getAsBinary();
+                this.output += "\r\n";
             } else {
-                if(typeof val == "object" && "fileName" in val) {
-                    this.output += "--" + this.boundary + "\r\n";
-                    this.output += "Content-Disposition: form-data; name=\"" + name.replace(/[^\w]/g, "") + "\";";
+                this.output += "--" + this.boundary + "\r\n";
+                this.output += "Content-Disposition: form-data; name=\""+name+"\";";
 
-                    // Encoding trick via ecmanaut (http://bit.ly/6p30c5)
-                    this.output += " filename=\""+unescape(encodeURIComponent(val.fileName)) +"\";\r\n";
-                    this.output += "Content-Type: " + val.type;
-
-                    this.output += "\r\n\r\n";
-                    this.output += val.getAsBinary();
-                    this.output += "\r\n";
-                } else {
-                    this.output += "--" + this.boundary + "\r\n";
-                    this.output += "Content-Disposition: form-data; name=\""+name+"\";";
-
-                    this.output += "\r\n\r\n";
-                    this.output += "" + val; // Force it into a string.
-                    this.output += "\r\n";
-                }
-            }
-        }
-
-        this.open = function(mode, url, bool, login, pass) {
-            this.xhr.open(mode, url, bool, login, pass);
-        }
-
-        this.send = function() {
-            if(hasFormData) {
-                this.xhr.send(this.formData);
-            } else {
-                content_type = "multipart/form-data;boundary=" + this.boundary;
-                this.xhr.setRequestHeader("Content-Type", content_type);
-
-                this.output += "--" + this.boundary + "--";
-                this.xhr.sendAsBinary(this.output);
+                this.output += "\r\n\r\n";
+                this.output += "" + val; // Force it into a string.
+                this.output += "\r\n";
             }
         }
     }
-})();
+
+    this.open = function(mode, url, bool, login, pass) {
+        this.xhr.open(mode, url, bool, login, pass);
+    }
+
+    this.send = function() {
+        if(this.hasFormData) {
+            this.xhr.send(this.formData);
+        } else {
+            content_type = "multipart/form-data;boundary=" + this.boundary;
+            this.xhr.setRequestHeader("Content-Type", content_type);
+
+            this.output += "--" + this.boundary + "--";
+            this.xhr.sendAsBinary(this.output);
+        }
+    }
+}
 
